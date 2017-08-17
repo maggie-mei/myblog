fdc84505389d883d728473b419af210aef25b5ad
guohzou <guoz@idreamtech.com.cn>
2017-08-17 10:32:16 +0800

    辣鸡links个大傻逼说我侵犯他的肖像权要老子删掉

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,48 @@
-$(function(){
-	$.ajax({
-		url:'/api/getUser',
-	    data:{},
-	    dataType:'json',
-	    type:'get',
-	    success:function(res){
-	        if(res.status == 200){
-	        	$('.user').text(res.data.user)
-	        }
-	    }
-	})
+$(function() {
+
+  $.ajax({
+    url: '/api/getUser',
+    data: {},
+    dataType: 'json',
+    type: 'get',
+    success: function(res) {
+      if (res.status == 200) {
+        $('.user').text(res.data.user)
+      }
+    }
+  })
   $.fn.extend({
-    tipOpen:function(str){
-      if($(this).hasClass('tip')){
-        var html = '<p class="tip-content">'+str+'</p>';
+    tipOpen: function(str) {
+      if ($(this).hasClass('tip')) {
+        var html = '<p class="tip-content">' + str + '</p>';
         $(this).append(html)
       }
-      $(this).on('mouseout',function(){
+      $(this).on('mouseout', function() {
         $(this).find('p').remove();
       })
     }
   })
   $.ajax({
-    url:"/api/getUserInfo",
-    type:"get",
-    success:function(res){
-      if(res.status == 200){
-        $('.icon img').prop('src',res.data.icon);
+    url: "/api/getUserInfo",
+    type: "get",
+    success: function(res) {
+      if (res.status == 200) {
+        // $('.icon img').prop('src',res.data.icon);
+        var img = $('.icon img')[0];
+        img.src = './img/code.png'
+        var newImg = new Image();
+        newImg.src = res.data.icon;
+        newImg.onerror = () => { // 图片加载错误时的替换图片
+          newImg.src = './img/code.png';
+        }
+        newImg.onload = () => { // 图片加载成功后把地址给原来的img
+          img.src = newImg.src
+        }
         $('.user-name').text(res.data.name);
         // $('.user-name').on('mouseover',function(){
         //   $(this).tipOpen('哈哈')
         // })
-      }else{
+      } else {
         location.href = 'login'
       }
     }
